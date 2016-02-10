210b3916fd7f722ecb4efde1e5780939b32c7f49
Jukino <acazuc@student.42.fr>
2016-02-10 17:08:58 +0100

    Fixed sort_3

--- srcs/sort_3.c
+++ srcs/sort_3.c
@@ -6,7 +6,7 @@
 /*   By: acazuc <acazuc@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/09 09:51:22 by acazuc            #+#    #+#             */
-/*   Updated: 2016/02/09 10:04:39 by acazuc           ###   ########.fr       */
+/*   Updated: 2016/02/10 17:08:16 by acazuc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,27 +15,27 @@
 void	sort_3(t_env *env)
 {
 	if (env->stack_a[0] < env->stack_a[1]
-			&& env->stack_a[1] > env->stack_a[2]
-			&& env->stack_a[2] > env->stack_a[0])
-		sa(env);
-	else if (env->stack_a[0] > env->stack_a[1]
-			&& env->stack_a[1] > env->stack_a[2]
-			&& env->stack_a[2] < env->stack_a[0])
+		&& env->stack_a[1] > env->stack_a[2]
+		&& env->stack_a[2] < env->stack_a[0])
 	{
-		sa(env);
 		rra(env);
+		sa(env);
 	}
 	else if (env->stack_a[0] > env->stack_a[1]
-			&& env->stack_a[1] < env->stack_a[2]
-			&& env->stack_a[2] < env->stack_a[0])
-		rra(env);
+		&& env->stack_a[1] < env->stack_a[2]
+		&& env->stack_a[2] < env->stack_a[0])
+		sa(env);
 	else if (env->stack_a[0] < env->stack_a[1]
-			&& env->stack_a[1] > env->stack_a[2]
-			&& env->stack_a[2] < env->stack_a[0])
+		&& env->stack_a[1] > env->stack_a[2]
+		&& env->stack_a[2] > env->stack_a[0])
+		rra(env);
+	else if (env->stack_a[0] > env->stack_a[1]
+		&& env->stack_a[1] < env->stack_a[2]
+		&& env->stack_a[2] > env->stack_a[0])
 		ra(env);
 	else
 	{
 		sa(env);
-		ra(env);
+		rra(env);
 	}
 }
